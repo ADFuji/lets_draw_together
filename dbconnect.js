2df9f75a04e9f11f716e95bbb7b19baa15ddeac4
Fuji <84814195+ADFuji@users.noreply.github.com>
2022-10-05 04:39:01 +0200

    Delete dbconnect.js

--- dbconnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
-
-const url = "mongodb://localhost:27017/lets_draw_together";
-
-const connect = mongoose.connect(url);
-
-module.exports = connect;
